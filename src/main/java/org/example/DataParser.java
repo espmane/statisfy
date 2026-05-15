5dceac17ab3aee250c071ab7d9bf6821b30ebd77
esp <183955049+espmane@users.noreply.github.com>
2026-05-16 00:12:35 +0200

    files now throws IOException when null

--- src/main/java/org/example/DataParser.java
+++ src/main/java/org/example/DataParser.java
@@ -12,8 +12,7 @@ import com.google.gson.reflect.TypeToken;
 
 public class DataParser {
     private static final Gson GSON = new Gson();
-    private static final Type LIST_TYPE = new TypeToken<List<Song>>() {
-    }.getType();
+    private static final Type LIST_TYPE = new TypeToken<List<Song>>() {}.getType();
 
     public List<Song> getSongs(final String folderPath) throws IOException {
         final var folder = new File(folderPath);
@@ -23,21 +22,21 @@ public class DataParser {
             throw new IllegalArgumentException("Not a directory: " + folderPath);
         }
 
-
         final File[] files = folder.listFiles((file -> file.isFile() && file.getName().endsWith(".json")));
-        if (files != null) {
-            for (final File file : files) {
-                try {
-                    final String content = Files.readString(file.toPath());
-                    final List<Song> parsed = GSON.fromJson(content, LIST_TYPE);
-                    if (parsed != null) {
-                        songs.addAll(parsed);
-                    }
-                } catch (final IOException e) {
-                    throw new IOException("Failed to read file: " + file.getPath(), e);
+        if (files == null) throw new IOException("Failed to list files in: " + folderPath);
+
+        for (final File file : files) {
+            try {
+                final String content = Files.readString(file.toPath());
+                final List<Song> parsed = GSON.fromJson(content, LIST_TYPE);
+                if (parsed != null) {
+                    songs.addAll(parsed);
                 }
+            } catch (final IOException e) {
+                throw new IOException("Failed to read file: " + file.getPath(), e);
             }
         }
+
         return songs;
     }
 }
