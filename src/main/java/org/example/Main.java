2ac885224d85b79082cb80aa4b309d415a43dbe7
esp <183955049+espmane@users.noreply.github.com>
2026-05-16 02:26:19 +0200

    refactor DatabaseConnection to accept stats and integrate StatsRunner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,14 @@
 package org.example;
 
 import org.example.db.DatabaseConnection;
+import org.example.stats.*;
 
 public class Main {
-    private static final String jsonPath = "/home/esp/Downloads/spotify";
-    private static final String dbPath = "my.db"; // need to be changed to absolute path
+    private static final String JSON_PATH = "/home/esp/Downloads/spotify";
+    private static final String DB_PATH = "my.db";
 
     static void main() {
-        final var db = new DatabaseConnection(dbPath);
-        db.connect(jsonPath);
+        final var stats = StatCollector.of(ArtistStats.class, BehaviourStats.class);
+        new DatabaseConnection(DB_PATH, stats).connect(JSON_PATH);
     }
 }
--- src/main/java/org/example/db/DatabaseConnection.java
+++ src/main/java/org/example/db/DatabaseConnection.java
@@ -2,20 +2,24 @@ package org.example.db;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Set;
 
 import org.example.parser.DataParser;
+import org.example.stats.Stat;
 
 public class DatabaseConnection {
     private final String jdbcUrl;
     private final SongWriter songWriter;
     private final DatabaseManager dbManager;
     private final DataParser dataParser;
+    private final Set<Stat> stats;
 
-    public DatabaseConnection(final String dbPath) {
+    public DatabaseConnection(final String dbPath, final Set<Stat> stats) {
         this.jdbcUrl = "jdbc:sqlite:" + dbPath;
         this.dataParser = new DataParser();
         this.songWriter = new SongWriter();
         this.dbManager = new DatabaseManager();
+        this.stats = stats;
     }
 
     public void connect(final String jsonPath) {
@@ -25,6 +29,7 @@ public class DatabaseConnection {
                 final var songs = dataParser.getSongs(jsonPath);
                 dbManager.createTable(connection);
                 songWriter.insertSongs(connection, songs);
+                new StatsRunner(connection, stats).run();
             } catch (final Exception e) {
                 connection.rollback();
                 System.err.println(e.getMessage());
